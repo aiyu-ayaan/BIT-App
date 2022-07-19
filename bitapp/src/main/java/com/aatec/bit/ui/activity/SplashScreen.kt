87b944f997c46c4676db86f6675eeb18a7df52e5
Ayaan <aiyuayaan22@gmail.com>
2022-07-19 10:21:57 +0530

    Changes :-  * Logo change

--- bitapp/src/main/java/com/aatec/bit/ui/activity/SplashScreen.kt
+++ bitapp/src/main/java/com/aatec/bit/ui/activity/SplashScreen.kt
@@ -3,16 +3,12 @@ package com.aatec.bit.ui.activity
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.findNavController
-import com.aatec.bit.R
 import com.aatec.bit.ui.activity.main_activity.MainActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
