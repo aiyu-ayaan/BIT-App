1a1f47122e66332be013e2ada7b928a3d1107122
Ayaan <76834976+aiyu-ayaan@users.noreply.github.com>
2023-05-14 21:30:52 +0530

    Gradle downgrade to 7.4.2 (#134)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ buildscript {
 
 }
 plugins {
-    id ("com.android.application") version "8.2.0-alpha02" apply false
-    id ("com.android.library") version "8.2.0-alpha02" apply false
+    id ("com.android.application") version "7.4.2" apply false
+    id ("com.android.library") version "7.4.2" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("com.google.dagger.hilt.android") version "2.46" apply false
 }
