ef36ed3cd1496c6f48d9f3c57904bfa388bfc234
aiyu-ayaan <aiyuayaan22@gmail.com>
2023-06-30 01:32:01 +0530

    Working on HomeFragment.kt

--- bitapp/src/main/java/com/atech/bit/ui/activities/main_activity/MainActivity.kt
+++ bitapp/src/main/java/com/atech/bit/ui/activities/main_activity/MainActivity.kt
@@ -3,10 +3,12 @@ package com.atech.bit.ui.activities.main_activity
 import android.os.Bundle
 import android.viewbinding.library.activity.viewBinding
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.atech.bit.R
 import com.atech.bit.databinding.ActivityMainBinding
+import com.atech.bit.utils.onDestinationChange
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,7 +25,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.apply {
-            bottomNavigation.setupWithNavController(navController)
+            bottomNavigationSetup()
         }
+        handleDestinationChange()
+    }
+
+    private fun ActivityMainBinding.bottomNavigationSetup() {
+        bottomNavigation.setupWithNavController(navController)
+        bottomNavigation.setOnItemReselectedListener { }
+    }
+
+    private fun handleDestinationChange() {
+        navController.onDestinationChange { destination ->
+            when (destination.id) {
+                in baseFragments() -> setBottomNavigationVisibility(true)
+                else -> setBottomNavigationVisibility(false)
+            }
+        }
+    }
+
+    fun setBottomNavigationVisibility(isVisible: Boolean) {
+        binding.bottomNavigation.isVisible = isVisible
+    }
+
+    private fun baseFragments() =
+        listOf(
+            R.id.homeFragment,
+            com.atech.course.R.id.courseFragment
+        )
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- bitapp/src/main/java/com/atech/bit/ui/fragments/home/HomeFragment.kt
+++ bitapp/src/main/java/com/atech/bit/ui/fragments/home/HomeFragment.kt
@@ -3,19 +3,85 @@ package com.atech.bit.ui.fragments.home
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.atech.bit.R
 import com.atech.bit.databinding.FragmentHomeBinding
+import com.atech.bit.ui.activities.main_activity.MainActivity
+import com.atech.theme.customBackPress
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.search.SearchView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding: FragmentHomeBinding by viewBinding()
 
+    private val mainActivity: MainActivity by lazy {
+        requireActivity() as MainActivity
+    }
+    private val drawerLayout: DrawerLayout by lazy {
+        mainActivity.findViewById(R.id.drawerLayout)
+    }
+    private val navView: NavigationView by lazy {
+        mainActivity.findViewById(R.id.navigationView)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+            setDrawerOpen()
+        }
+        handleDrawer()
+        handleExpand()
+        handleBackPress()
+    }
 
+    private fun FragmentHomeBinding.setDrawerOpen() = this.searchBar.setNavigationOnClickListener {
+        setDrawerState(true)
+    }
+
+
+    private fun handleDrawer() = navView.apply {
+        setNavigationItemSelectedListener {
+            setDrawerState(false)
+            true
+        }
+    }
+
+    private fun setDrawerState(isOpen: Boolean) = this.drawerLayout.apply {
+        if (isOpen) openDrawer(GravityCompat.START)
+        else closeDrawer(GravityCompat.START)
+    }
+
+
+    private fun handleExpand() {
+        binding.searchView.addTransitionListener { _, _, newState ->
+            if (newState == SearchView.TransitionState.SHOWING) {
+                mainActivity.setBottomNavigationVisibility(false)
+            }
+            if (newState == SearchView.TransitionState.HIDING) {
+                mainActivity.setBottomNavigationVisibility(true)
+            }
+        }
+    }
+
+    private fun handleBackPress() {
+        customBackPress {
+            when {
+                drawerLayout.isDrawerOpen(GravityCompat.START) -> {
+                    setDrawerState(false)
+                }
+
+                else -> {
+                    if (binding.searchView.isShowing) {
+                        binding.searchView.hide()
+                    } else {
+                        activity?.finish()
+                    }
+                }
+            }
         }
     }
 
