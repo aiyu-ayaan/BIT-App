61783e3972a0dd4feee5ef55468795c4fbc4420e
aiyu-ayaan <aiyuayaan22@gmail.com>
2023-12-09 10:14:45 +0530

    Removing files for Compose Integration

--- theme/src/main/java/com/atech/theme/CoroutineException.kt
+++
@@ -1,9 +0,0 @@
-package com.atech.theme
-
-import android.util.Log
-import kotlinx.coroutines.CoroutineExceptionHandler
-
-private const val TAG = "CoroutineException"
-val handler = CoroutineExceptionHandler { _, throwable ->
-    Log.e(TAG, "Exception : $throwable ")
-}
